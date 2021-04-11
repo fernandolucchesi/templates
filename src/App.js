b88ca9511fde3d3207d7a978480425901d445254
Fernando Lucchesi <fernando@lexolve.com>
2021-04-11 19:00:04 +0200

    Remote agro website

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import React, { useState } from 'react';
 import TemplateOne from './TemplateOne';
 import TemplateTwo from './TemplateTwo';
 import TemplateThree from './TemplateThree';
+import TemplateFour from './TemplateFour';
 
 function App() {
   const [websiteOne, showWebsiteOne] = useState(false);
   const [websiteTwo, showWebsiteTwo] = useState(false);
   const [websiteThree, showWebsiteThree] = useState(true);
 
-  return <TemplateThree />;
+  return <TemplateFour />;
 }
 
 export default App;
